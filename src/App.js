2c1589cd9fa8296c645c23543028ef758266595b
Max <Serenity09@users.noreply.github.com>
2021-07-04 10:47:39 -0400

    V1
    UI & logic for core logic
    Seems to be working pretty well

--- src/App.js
+++ src/App.js
@@ -1,24 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+
+import './app.scss';
+
+import { stringInterpolate, cleanStringSQL, stringifyTemplate } from './StringExtensions'
 
 function App() {
+  const [ stringSQL, setStringSQL ] = useState("");
+  const stringSQLRef = useRef(null);
+
+  const [ templateSQL, setTemplateSQL ] = useState("");
+  const templateSQLRef = useRef(null);
+  const [ anonymousReferences, setAnonymousReferences ] = useState([]);
+
+  const [ nativeSQL, setNativeSQL ] = useState("");
+
+  const [ activeEditor, setActiveEditor ] = useState(null);
+
+  const [ rootContext, setRootContext ] = useState({
+    siteCD: "PV.SITE_CD = 640",
+    COVID_PCR_TEST_NAMES: ["'COVID-19 (PHRL)'", "'COVID-19 (VAPAHCS-ABBOTT)'", "'COVID-19 (VAPAHCS-CEPHEID)'", "'COVID-19 CONFIRMATORY (PHRL)'",
+      "'COVID-19 CONFIRMATORY (VAPACHS-ABBOTT)'", "'COVID-19 CONFIRMATORY (VAPAHCS-CEPHEID)'",
+      "'NOVEL CORONAVIRUS 2019 RRT PCR (PHRL)'", "'COVID-19 (ABBOTT)'", "'COVID-19 (BIOFIRE)'", "'COVID-19 (CEPHEID)'",
+      "'COVID-19 (PHRL8761)'", "'COVID-19 PCR (FLUVID)'"
+    ],
+    covidStatusExpr:
+      "CASE " +
+          "WHEN POS_BEFORE_ADM.PTNT_IDNTFR IS NOT NULL OR POS_DURING_ADM.PTNT_IDNTFR IS NOT NULL THEN 'COVID+' " +
+          "WHEN PUI.PTNT_IDNTFR IS NOT NULL THEN 'PUI' " +
+          "ELSE 'Non-COVID' " +
+      "END",
+    dateExpr: "TO_CHAR(TRUNC(ADM.ADMSN_DT, 'IW') - 1, 'YYYY-MM-DD')",
+    getPatientTypeFilterExpr: "AND ((P.PSBLY_VTRN_FLG = 'Y' AND P.PSBLY_VTRN_EMP_FLG = 'N' AND P.PSBLY_EMP_FLG = 'N') OR ((P.PSBLY_VTRN_FLG = 'Y' AND P.PSBLY_EMP_FLG = 'Y') OR P.PSBLY_VTRN_EMP_FLG = 'Y') OR (P.PSBLY_EMP_FLG = 'Y' AND P.PSBLY_VTRN_FLG = 'N' AND P.PSBLY_VTRN_EMP_FLG = 'N'))",
+
+    anonymousParameters: [
+      640,
+      640,
+      "'2020-01-01 00:00:00'",
+      "'2021-12-31 23:59:59'"
+    ],
+
+    stringJoinImplementation: "line-ending"
+  });
+
+  const onStringSQLChange = function(evt) {
+    setStringSQL(evt.target.value);
+  }
+
+  // const START_DELIMITER = "${";
+  // const END_DELIMITER = "}";
+
+  // const ANONYMOUS_PARAMETER = "?";
+
+  useEffect(() => {
+    // const stringifyTemplateSQL = function(value) {
+    //   const splitTemplateSQL = value.split("\n");
+    //   const stringifiedTemplateSQL = [];
+  
+    //   if (splitTemplateSQL.some((value) => {
+    //     return value;
+    //   })) {
+    //     splitTemplateSQL.forEach((templateSQLLine) => {  
+    //       const stringifiedTemplateSQLLine = [];
+
+    //       if (templateSQLLine) {
+    //         let iTemplateCharacterIndex = 0;
+    //         let startCopyIndex = 0;
+
+    //         const templateParts = [];
+    //         const template = templateSQLLine;
+
+    //         while (iTemplateCharacterIndex < template.length) {
+    //             const endDelimiterIndex = template.indexOf(END_DELIMITER, iTemplateCharacterIndex);
+
+    //             //check if the current template index is greater than the minimum number of characters to represent a template variable
+    //             if (iTemplateCharacterIndex >= template.length - START_DELIMITER.length - END_DELIMITER.length - 1) {
+    //                 templateParts.push("\"" + template.substring(startCopyIndex) + "\"");
+    //                 iTemplateCharacterIndex = template.length;
+    //             }
+    //             //otherwise check if the sequence at the current index matches the START_DELIMITER and that the END_DELIMITER occurs someplace after the current index
+    //             else if (template.substring(iTemplateCharacterIndex, iTemplateCharacterIndex + START_DELIMITER.length) === START_DELIMITER && endDelimiterIndex >= 0) {
+    //                 if (startCopyIndex !== iTemplateCharacterIndex) {
+    //                     templateParts.push("\"" + template.substring(startCopyIndex, iTemplateCharacterIndex) + "\"");
+
+    //                     startCopyIndex = endDelimiterIndex + END_DELIMITER.length;
+    //                 }
+
+    //                 templateParts.push(template.substring(iTemplateCharacterIndex + START_DELIMITER.length, endDelimiterIndex));
+
+    //                 iTemplateCharacterIndex = endDelimiterIndex + END_DELIMITER.length;
+    //             }
+    //             else
+    //             //otherwise move on to the next character
+    //                 iTemplateCharacterIndex++;
+    //         }
+
+    //         let strTemplateSQL;
+    //         if (templateParts.length > 0)
+    //           strTemplateSQL = templateParts.join(" + ");
+    //         else
+    //           strTemplateSQL = "";
+                        
+    //         stringifiedTemplateSQLLine.push(strTemplateSQL);
+    //       }
+  
+    //       if (stringifiedTemplateSQLLine.length > 0) {
+    //         const joinedStrTemplateSQL = stringifiedTemplateSQLLine.join(" + ");
+            
+    //         stringifiedTemplateSQL.push(joinedStrTemplateSQL);
+    //       }
+    //     });
+    //   }
+  
+    //   return stringifiedTemplateSQL.join(" + \"\\n\" +\n");
+    // }
+    
+    if (activeEditor !== null && templateSQLRef.current === activeEditor) {
+      const joinedStringSQL = stringifyTemplate(templateSQL);
+      setStringSQL(joinedStringSQL);
+    }
+
+    const nativeSQL = stringInterpolate(templateSQL, rootContext);
+    // const nativeSQL2 = fillTemplate(templateSQL, rootContext);
+    // console.log("nativeSQL2", nativeSQL2);
+    setNativeSQL(nativeSQL);
+  }, [activeEditor, rootContext, templateSQL]);
+
+  const onTemplateSQLChange = function(evt) {
+    setTemplateSQL(evt.target.value);
+  }
+
+  useEffect(() => {
+    const stringToTemplateSQL = function(value) {
+      const splitStringSQL = value.split("\n");
+      const templateSQLLines = [];
+  
+      if (splitStringSQL[0]) {
+        splitStringSQL.forEach((stringSQLLine, lineNumber) => {  
+          const templateSQLLine = [];
+
+          if (stringSQLLine) {
+            const strTemplateSQL = cleanStringSQL(stringSQLLine, rootContext.anonymousParameters);
+            templateSQLLine.push(strTemplateSQL);
+          }
+  
+          if (templateSQLLine.length > 0) {
+            const joinedTemplateSQL = templateSQLLine.join(" ");
+            templateSQLLines.push(joinedTemplateSQL);
+          }
+        });
+      }
+  
+      return templateSQLLines.join("\n");
+    }
+    
+    if (activeEditor !== null && stringSQLRef.current === activeEditor) {
+      const cleanedSQL = stringToTemplateSQL(stringSQL);
+
+      console.log("string SQL changed", stringSQL);
+      console.log("cleaned", cleanedSQL);
+      //TODO update template string editor based on string SQL
+      setTemplateSQL(cleanedSQL);
+    }
+  }, [activeEditor, rootContext.anonymousParameters, stringSQL]);
+
+  const onFocus = function(evt) {
+    setActiveEditor(evt.target);
+  }
+  const onBlur = function(evt) {
+    setActiveEditor(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="h-100">
+      <Container className="app-container h-100" fluid>
+        <h1 className="mb-3">string-sql-editor</h1>
+        <Row className="editor-row">
+          <Col>
+            <Form.Group controlId="applicationStringSQLInput" className="h-100">
+              <Form.Label>IO Application String SQL</Form.Label>
+              <Form.Control ref={stringSQLRef} as="textarea" placeholder="Copy and paste application string SQL to and from here"
+                value={stringSQL}
+                onChange={onStringSQLChange}
+                onFocus={onFocus}
+                onBlur={onBlur} />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="applicationStringSQLInput" className="h-100">
+              <Form.Label>Template SQL Editor</Form.Label>
+              <Form.Control ref={templateSQLRef} as="textarea" placeholder="Edit a more minimal view of application string SQL here. &#10;This view allows for references to Java variables available in the runtime context"
+                value={templateSQL}
+                onChange={onTemplateSQLChange}
+                onFocus={onFocus}
+                onBlur={onBlur} />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="applicationStringSQLInput" className="h-100">
+              <Form.Label>SQL View</Form.Label>
+              <Form.Control as="textarea" placeholder="Valid SQL will be generated here. &#10;References to Java variables will be replaced using the selected context" readOnly
+                value={nativeSQL} />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Container>
+    </Form>
   );
 }
 
---
+++ src/StringExtensions.js
@@ -0,0 +1,169 @@
+const START_DELIMITER = "${";
+const END_DELIMITER = "}";
+
+const ANONYMOUS_PARAMETER = "?";
+
+const stringifyTemplate = function (template) {
+    const splitTemplateSQL = template.split("\n");
+    const stringifiedTemplateSQL = [];
+
+    //check if there is any line in the template string that's not null or empty and, if there is, iterate all elements
+    if (splitTemplateSQL.some((value) => {
+        return value;
+    })) {
+        splitTemplateSQL.forEach((templateSQLLine, iLineNumber) => {  
+            let iLineCharacterIndex = 0;
+            let startCopyIndex = 0;
+
+            const templateParts = [];
+
+            while (iLineCharacterIndex < templateSQLLine.length) {
+                const endDelimiterIndex = templateSQLLine.indexOf(END_DELIMITER, iLineCharacterIndex);
+
+                //check if the current template index is greater than the minimum number of characters to represent a template variable
+                if (iLineCharacterIndex === templateSQLLine.length - 1) {
+                    templateParts.push("\"" + templateSQLLine.substring(startCopyIndex) + "\"");
+                    iLineCharacterIndex = templateSQLLine.length;
+                }
+                //otherwise check if the sequence at the current index matches the START_DELIMITER and that the END_DELIMITER occurs someplace after the current index
+                else if (templateSQLLine.substring(iLineCharacterIndex, iLineCharacterIndex + START_DELIMITER.length) === START_DELIMITER && endDelimiterIndex >= 0) {
+                    if (startCopyIndex !== iLineCharacterIndex) {
+                        templateParts.push("\"" + templateSQLLine.substring(startCopyIndex, iLineCharacterIndex) + "\"");
+                    }
+
+                    templateParts.push(templateSQLLine.substring(iLineCharacterIndex + START_DELIMITER.length, endDelimiterIndex));
+
+                    iLineCharacterIndex = endDelimiterIndex + END_DELIMITER.length;
+                    startCopyIndex = endDelimiterIndex + END_DELIMITER.length;
+                }
+                else
+                    //otherwise move on to the next character
+                    iLineCharacterIndex++;
+            }
+
+            let strTemplateSQL;
+            if (templateParts.length > 0) {
+                if (iLineNumber > 0)
+                    templateParts.splice(0, 0, "");
+                
+                strTemplateSQL = templateParts.join(" + ");
+            }
+            else
+                strTemplateSQL = "";
+
+            stringifiedTemplateSQL.push(strTemplateSQL);
+        });
+    }
+    else
+        return "";
+
+    //const rejoinedTemplateSQL = stringifiedTemplateSQL.join(" + \"\\n\"\n");
+    const rejoinedTemplateSQL = stringifiedTemplateSQL.map((sqlLine, iLineNumber) => {
+        if (iLineNumber === 0 && !sqlLine)
+            return sqlLine + "\"\\n\"\n";
+        else
+            return sqlLine + " + \"\\n\"\n";
+    }).join("");
+    
+    return rejoinedTemplateSQL;
+}
+
+const stringInterpolate = function (template, params) {
+    let interpolatedString = "";
+    let iTemplateCharacterIndex = 0;
+
+    let iAnonymousParameter = 0;
+
+    //replace ? characters
+    while (iTemplateCharacterIndex < template.length) {
+        const endDelimiterIndex = template.indexOf(END_DELIMITER, iTemplateCharacterIndex);
+
+        if (template.substring(iTemplateCharacterIndex, iTemplateCharacterIndex + ANONYMOUS_PARAMETER.length) === ANONYMOUS_PARAMETER) {
+            const anonymousParameter = params.anonymousParameters[iAnonymousParameter];
+            interpolatedString += anonymousParameter;
+
+            iAnonymousParameter++;
+            iTemplateCharacterIndex += ANONYMOUS_PARAMETER.length;
+        }
+        else if (template.substring(iTemplateCharacterIndex, iTemplateCharacterIndex + START_DELIMITER.length) === START_DELIMITER && endDelimiterIndex >= 0) {
+            const keyStartIndex = iTemplateCharacterIndex + START_DELIMITER.length;
+            const keyEndIndex = endDelimiterIndex;
+
+            let originalKey = template.substring(keyStartIndex, keyEndIndex);
+            let strippedKey = originalKey;
+            if (originalKey.substring(originalKey.length - "()".length) === "()")
+                strippedKey = originalKey.substring(0, originalKey.length - "()".length);
+
+            if (strippedKey in params) {
+                interpolatedString += params[strippedKey];
+            }
+            else {
+                interpolatedString += template.substring(iTemplateCharacterIndex + START_DELIMITER.length, endDelimiterIndex);
+            }
+
+            iTemplateCharacterIndex = endDelimiterIndex + END_DELIMITER.length;
+        }
+        else {
+            interpolatedString += template[iTemplateCharacterIndex];
+            iTemplateCharacterIndex++;
+        }
+    }
+
+    return interpolatedString;
+};
+
+const variableNameRegExp = new RegExp("[a-zA-Z]", "g");
+const getStringSQLVariableName = function(stringSQL) {
+    const variableNameStartIndex = stringSQL.search(variableNameRegExp);
+    let variableName = "";
+
+    if (variableNameStartIndex >= 0) {
+        let variableNameEndIndex = stringSQL.indexOf(" ", variableNameStartIndex + 1);
+        if (variableNameEndIndex === -1)
+            variableNameEndIndex = stringSQL.length;
+
+        variableName = stringSQL.substring(variableNameStartIndex, variableNameEndIndex);
+    }
+
+    return variableName
+}
+const wrapStringSQLVariable = function(stringSQL, candidateStartIndex, candidateEndIndex) {
+    const candidateString = stringSQL.substring(candidateStartIndex, candidateEndIndex);
+    const variableName = getStringSQLVariableName(candidateString);
+
+    if (variableName) {
+        const variableStartIndex = stringSQL.indexOf(variableName, candidateStartIndex);
+                
+        const wrappedVariableName = "${" + variableName + "}";
+        
+        stringSQL = stringSQL.substring(0, variableStartIndex) + wrappedVariableName + stringSQL.substring(variableStartIndex + variableName.length);
+    }
+
+    return stringSQL;
+}
+
+const cleanStringSQL = function (stringSQL) {
+    //replace string segments which are not wrapped in quotation marks
+    const splitStringSQL = stringSQL.split("+");
+    splitStringSQL.forEach((splitStringSQLElement, index) => {
+        const trimmedStringSQLElement = splitStringSQLElement.trim();
+
+        if (trimmedStringSQLElement.length > 0 && trimmedStringSQLElement.charAt(0) !== "\"" && trimmedStringSQLElement.charAt(trimmedStringSQLElement.length - 1) !== "\"") {
+            const wrappedTemplateSQL = wrapStringSQLVariable(splitStringSQLElement, 0, splitStringSQLElement.length);
+            splitStringSQL[index] = wrappedTemplateSQL;
+        }
+    });
+
+    stringSQL = splitStringSQL.join("+");
+    
+    return stringSQL
+        .replace(new RegExp("\\\\r", "g"), "")
+        .replace(new RegExp("\\\\n", "g"), "")
+        .replace(new RegExp("\\ \\+\\ ", "g"), "")
+        .replace(new RegExp("\\ \\+", "g"), "")
+        .replace(new RegExp("\"", "g"), "")
+    
+    // return stringSQL.replace(new RegExp("^(\\\\r|\\\\n|\\ \\+\\ |\\ \\+|\"|\\(|\\))", "g"), "")
+};
+
+export { stringInterpolate, stringifyTemplate, cleanStringSQL };
